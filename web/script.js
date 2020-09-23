745e965b407779612c870c0235b06a5d9efd766f
Erik Butcher <with_a_k@mac.com>
2020-09-23 13:43:07 -0600

    Add JQuery

--- web/script.js
+++ web/script.js
@@ -5,6 +5,22 @@ function alertMe() {
 function colorChange() {
   var ourDiv = document.getElementById('grid-column1');
   var ourColor = document.getElementById('color-field');
-  console.log(ourColor);
   ourDiv.style.backgroundColor = ourColor.value;
 }
+
+function fadeJQ() {
+  $("#grid-column3").hide("slow");
+  $("#fade-button").on("click", unfadeJQ());
+}
+
+function unfadeJQ() {
+  $("#grid-column3").show("slow");
+  $("#fade-button").on("click", fadeJQ());
+}
+
+//Don't let these work until the document is ready
+$(function() {
+  //I figure there's no sense rewriting it.
+  $("#color-button").on("click", colorChange());
+  $("#fade-button").on("click", fadeJQ());
+});
